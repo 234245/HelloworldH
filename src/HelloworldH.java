34ed5c02f9f032a3379f1e356e69ab25d938cc27
hafsa234245 <hafsaakter7534@gmail.com>
2024-03-17 14:33:56 +0600

    Three variable getter setter method added of java project.

--- src/HelloworldH.java
+++ src/HelloworldH.java
@@ -1,5 +1,49 @@
 public class HelloworldH {
+    private int roll;
+    private String name;
+    private boolean passed;
+    public HelloworldH(){
+        roll = 100;
+        name = "CSE";
+        passed = true;
+    }
+    public HelloworldH(int roll, String name, boolean passed){
+        this.roll = roll;
+        this.name = name;
+        this.passed = passed;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello,worldH!");
+        HelloworldH helloWorld = new HelloworldH();
+        HelloworldH helloWorld2 = new HelloworldH(300,"SHU", true);
+        helloWorld.setName("ICE");
+        helloWorld.setRoll(400);
+        helloWorld.setPassed(false);
+        System.out.println(helloWorld2.getName());
+        System.out.println(helloWorld.getRoll());
+        System.out.println(helloWorld2.isPassed());
     }
 }
+
